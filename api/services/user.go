4e70526259c5850f4f884895bd631614b518abce
Michel Foucault <michel@condensat.tech>
2020-02-19 11:00:00 +0000

    Add user.Info Api

---
+++ api/services/user.go
@@ -0,0 +1,70 @@
+package services
+
+import (
+	"net/http"
+
+	"git.condensat.tech/bank/api/sessions"
+	"git.condensat.tech/bank/database"
+	"git.condensat.tech/bank/logger"
+
+	"github.com/sirupsen/logrus"
+)
+
+// KYCService receiver
+type UserService int
+
+// UserInfoRequest holds args for start requests
+type UserInfoRequest struct {
+	SessionArgs
+}
+
+// UserInfoResponse holds args for start requests
+type UserInfoResponse struct {
+	Email string `json:"email"`
+}
+
+// Info operation return user's email
+func (p *UserService) Info(r *http.Request, request *UserInfoRequest, reply *UserInfoResponse) error {
+	ctx := r.Context()
+	log := logger.Logger(ctx).WithField("Method", "services.UserService.Info")
+	log = GetServiceRequestLog(log, r, "User", "Info")
+
+	// Retrieve context values
+	_, session, err := ContextValues(ctx)
+	if err != nil {
+		log.WithError(err).
+			Error("ContextValues Failed")
+		return ErrServiceInternalError
+	}
+
+	// Get userID from session
+	sessionID := sessions.SessionID(request.SessionID)
+	userID := session.UserSession(ctx, sessionID)
+	if !sessions.IsUserValid(userID) {
+		log.Error("Invalid userSession")
+		return sessions.ErrInvalidSessionID
+	}
+	log = log.WithFields(logrus.Fields{
+		"SessionID": sessionID,
+		"UserID":    userID,
+	})
+
+	// Request UserID from email
+	user, err := database.FindUserById(ctx, userID)
+	if err != nil {
+		log.WithError(err).
+			Error("database.FindUserById Failed")
+		return ErrServiceInternalError
+	}
+
+	// Reply
+	*reply = UserInfoResponse{
+		Email: user.Email,
+	}
+
+	log.WithFields(logrus.Fields{
+		"Email": reply.Email,
+	}).Info("User started")
+
+	return nil
+}
