050297a77d38d8a719229c71cf69b83d66f1f8e1
Michel Foucault <michel@condensat.tech>
2020-03-19 08:00:00 +0000

    Fix too long log_entry.method

--- currency/rate/cmd/grabber/main.go
+++ currency/rate/cmd/grabber/main.go
@@ -34,7 +34,7 @@ type Args struct {
 func parseArgs() Args {
 	var args Args
 
-	appcontext.OptionArgs(&args.App, "CurrencyRateGrabber")
+	appcontext.OptionArgs(&args.App, "RateGrabber")
 
 	cache.OptionArgs(&args.Redis)
 	messaging.OptionArgs(&args.Nats)
