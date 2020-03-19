04687c17dce09c154770b55f226f76bb209073eb
Michel Foucault <michel@condensat.tech>
2020-03-19 10:42:54 +0100

    Add Currency model & tests

---
+++ database/model/currency.go
@@ -0,0 +1,17 @@
+package model
+
+type Currency struct {
+	Name      string `gorm:"primary_key;type:varchar(16)"` // [PK] Currency
+	Available *int   `gorm:"default:0"`
+}
+
+func NewCurrency(name string, available int) Currency {
+	return Currency{
+		Name:      name,
+		Available: &available,
+	}
+}
+
+func (p *Currency) IsAvailable() bool {
+	return p.Available != nil && *p.Available > 0
+}
