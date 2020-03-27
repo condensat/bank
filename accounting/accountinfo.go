bb4df20f8e7d73f473cdc9b817512b3f53e0c321
Michel Foucault <michel@condensat.tech>
2020-03-27 11:00:00 +0000

    Lock User and Account with mutex

--- accounting/accountinfo.go
+++ accounting/accountinfo.go
@@ -2,6 +2,7 @@ package accounting
 
 import (
 	"context"
+	"errors"
 	"time"
 
 	"git.condensat.tech/bank"
@@ -12,6 +13,12 @@ import (
 
 func ListUserAccounts(ctx context.Context, userID uint64) ([]AccountInfo, error) {
 	var result []AccountInfo
+
+	lock := LockUser(ctx, userID)
+	if lock == nil {
+		return result, errors.New("Failed to lock user")
+	}
+	defer lock.Unlock()
 
 	db := appcontext.Database(ctx)
 	err := db.Transaction(func(db bank.Database) error {
@@ -42,6 +49,11 @@ func ListUserAccounts(ctx context.Context, userID uint64) ([]AccountInfo, error)
 
 func GetAccountHistory(ctx context.Context, accountID uint64, from, to time.Time) ([]AccountEntry, error) {
 	var result []AccountEntry
+	lock := LockAccount(ctx, accountID)
+	if lock == nil {
+		return result, errors.New("Failed to lock account")
+	}
+	defer lock.Unlock()
 
 	db := appcontext.Database(ctx)
 	err := db.Transaction(func(db bank.Database) error {
