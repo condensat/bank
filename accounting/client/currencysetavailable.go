4793cd7d4ec96cb03b2ed10b0fb3f0085911b8d8
Michel Foucault <michel@condensat.tech>
2020-03-31 14:00:00 +0000

    Add CurrencySetAvailable handler & client

---
+++ accounting/client/currencysetavailable.go
@@ -0,0 +1,35 @@
+package client
+
+import (
+	"context"
+
+	"git.condensat.tech/bank/accounting/common"
+	"git.condensat.tech/bank/logger"
+	"git.condensat.tech/bank/messaging"
+
+	"github.com/sirupsen/logrus"
+)
+
+func CurrencySetAvailable(ctx context.Context, currencyName string, available bool) (common.CurrencyInfo, error) {
+	log := logger.Logger(ctx).WithField("Method", "Client.CurrencySetAvailable")
+
+	request := common.CurrencyInfo{
+		Name:      currencyName,
+		Available: available,
+	}
+
+	var result common.CurrencyInfo
+	err := messaging.RequestMessage(ctx, common.CurrencySetAvailableSubject, &request, &result)
+	if err != nil {
+		log.WithError(err).
+			Error("RequestMessage failed")
+		return common.CurrencyInfo{}, messaging.ErrRequestFailed
+	}
+
+	log.WithFields(logrus.Fields{
+		"Name":      result.Name,
+		"Available": result.Available,
+	}).Debug("Currency SetAvailable")
+
+	return result, nil
+}
