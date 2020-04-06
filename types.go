95d394dddf980724cfe8433e0b4262eac64245d6
Michel Foucault <michel@condensat.tech>
2020-04-06 08:00:00 +0000

    Add ToString and Parse to SecureID interface
    Remove helpers from service accounting

--- types.go
+++ types.go
@@ -66,4 +66,7 @@ type Worker interface {
 type SecureID interface {
 	ToSecureID(context string, value secureid.Value) (secureid.SecureID, error)
 	FromSecureID(context string, secureID secureid.SecureID) (secureid.Value, error)
+
+	ToString(secureID secureid.SecureID) string
+	Parse(secureID string) secureid.SecureID
 }
