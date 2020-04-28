8e8b21e522360b338198586c23197d37430c6b4e
Michel Foucault <michel@condensat.tech>
2020-04-28 12:00:00 +0000

    Add OperationInfo & OperationStatus data model

---
+++ database/model/operationstatus.go
@@ -0,0 +1,9 @@
+package model
+
+import "time"
+
+type OperationStatus struct {
+	OperationInfoID ID        `gorm:"unique_index;not null"`           // [FK] Reference to OperationInfo table
+	LastUpdate      time.Time `gorm:"index;not null;type:timestamp"`   // Last update timestamp
+	State           string    `gorm:"index;not null;type:varchar(16)"` // [enum] Operation synchroneous state (open, close)
+}
