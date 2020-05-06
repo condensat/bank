4bbaaf9f626f1cec6a8fbdc775487328c284af2a
Michel Foucault <michel@condensat.tech>
2020-05-06 08:00:00 +0000

    Add Asset to Wallet models

--- wallet/models.go
+++ wallet/models.go
@@ -9,5 +9,6 @@ func Models() []model.Model {
 	var result []model.Model
 	result = append(result, database.CryptoAddressModel()...)
 	result = append(result, database.OperationInfoModel()...)
+	result = append(result, new(model.Asset))
 	return result
 }
