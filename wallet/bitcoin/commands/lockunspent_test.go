c856e27140a2197ac7534f1ddf7ab454c403ac70
Michel Foucault <michel@condensat.tech>
2020-05-09 07:00:00 +0000

    Bitcoin Rpc tests does not fails if no server is not present
    - Rpc test are disabled by default

--- wallet/bitcoin/commands/lockunspent_test.go
+++ wallet/bitcoin/commands/lockunspent_test.go
@@ -21,7 +21,7 @@ func TestLockUnspent(t *testing.T) {
 	{
 		unspent, err := ListUnspent(ctx, rpcClient, nil)
 		if err != nil {
-			t.Errorf("ListUnspent failed")
+			t.Logf("ListUnspent failed")
 			return
 		}
 		for _, tx := range unspent {
