f8d822f3b46f590fb1551fd385ec33ff90845030
Michel Foucault <michel@condensat.tech>
2020-06-23 14:00:00 +0000

    Lock BatchNetwork while processing

--- wallet/tasks/batchwithdraw.go
+++ wallet/tasks/batchwithdraw.go
@@ -2,9 +2,15 @@ package tasks
 
 import (
 	"context"
+	"errors"
 	"time"
 
+	"git.condensat.tech/bank/cache"
 	"git.condensat.tech/bank/logger"
+)
+
+var (
+	ErrProcessingBatchWithdraw = errors.New("Error Processing BatchWithdraw")
 )
 
 func BatchWithdraw(ctx context.Context, epoch time.Time, chains []string) {
@@ -18,5 +24,26 @@ func processBatchWithdraw(ctx context.Context, epoch time.Time, chains []string)
 	for _, chain := range chains {
 		log = log.WithField("Chain", chain)
 		log.Debugf("Process Batch Withdraw")
+
+		err := processBatchWithdrawChain(ctx, chain)
+		if err != nil {
+			log.WithError(err).Error("Failed to processBatchWithdrawChain")
+			continue
+		}
 	}
+}
+
+func processBatchWithdrawChain(ctx context.Context, chain string) error {
+	log := logger.Logger(ctx).WithField("Method", "tasks.processBatchWithdrawChain")
+
+	// Acquire Lock
+	lock, err := cache.LockBatchNetwork(ctx, chain)
+	if err != nil {
+		log.WithError(err).
+			Error("Failed to lock batchNetwork")
+		return ErrProcessingBatchWithdraw
+	}
+	defer lock.Unlock()
+
+	return nil
 }
