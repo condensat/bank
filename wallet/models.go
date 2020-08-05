dd7dc8254fb8633dba5140c4068266653798b5b8
Michel Foucault <michel@condensat.tech>
2020-08-05 08:00:00 +0000

    Add SsmPool task to wallet

--- wallet/models.go
+++ wallet/models.go
@@ -8,6 +8,7 @@ import (
 func Models() []model.Model {
 	var result []model.Model
 	result = append(result, database.CryptoAddressModel()...)
+	result = append(result, database.SsmAddressModel()...)
 	result = append(result, database.OperationInfoModel()...)
 	result = append(result, database.AssetModel()...)
 	return result
