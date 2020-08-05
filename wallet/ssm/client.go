b33797eacc15990d55dd76249f389c352865f6e7
Michel Foucault <michel@condensat.tech>
2020-08-05 08:00:00 +0000

    Add PubKey & BlindingKey to SsmClient NewAddress
    - add SSmAddress type

--- wallet/ssm/client.go
+++ wallet/ssm/client.go
@@ -11,6 +11,7 @@ import (
 	"github.com/ybbus/jsonrpc"
 
 	"git.condensat.tech/bank/logger"
+	"git.condensat.tech/bank/wallet/common"
 	"git.condensat.tech/bank/wallet/rpc"
 	"git.condensat.tech/bank/wallet/ssm/commands"
 )
@@ -60,26 +61,33 @@ func NewWithTorEndpoint(ctx context.Context, endpoint string) *SsmClient {
 	}
 }
 
-func (p *SsmClient) NewAddress(ctx context.Context, ssmPath commands.SsmPath) (string, error) {
+func (p *SsmClient) NewAddress(ctx context.Context, ssmPath commands.SsmPath) (common.SsmAddress, error) {
 	log := logger.Logger(ctx).WithField("Method", "ssm.NewAddress")
 
 	client := p.client
 	if p.client == nil {
-		return "", ErrInternalError
+		return common.SsmAddress{}, ErrInternalError
 	}
 
 	result, err := commands.NewAddress(ctx, client.Client, ssmPath.Chain, ssmPath.Fingerprint, ssmPath.Path)
 	if err != nil {
 		log.WithError(err).Error("NewAddress failed")
-		return "", ErrRPCError
+		return common.SsmAddress{}, ErrRPCError
 	}
 
 	log.
 		WithField("Chain", result.Chain).
 		WithField("Address", result.Address).
+		WithField("PubKey", result.PubKey).
+		WithField("BlindingKey", result.BlindingKey).
 		Debug("SSM RPC")
 
-	return result.Address, nil
+	return common.SsmAddress{
+		Chain:       result.Chain,
+		Address:     result.Address,
+		PubKey:      result.PubKey,
+		BlindingKey: result.BlindingKey,
+	}, nil
 }
 
 func (p *SsmClient) SignTx(ctx context.Context, chain, inputransaction string, inputs ...commands.SignTxInputs) (string, error) {
