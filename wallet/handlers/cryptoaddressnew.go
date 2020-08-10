d36a6c02171af2407a6fc0cf88b6c12108a79150
Michel Foucault <michel@condensat.tech>
2020-08-10 15:00:00 +0000

    Remove db transaction for txNewCryptoAddressSsm
    - fix log typo

--- wallet/handlers/cryptoaddressnew.go
+++ wallet/handlers/cryptoaddressnew.go
@@ -170,7 +170,7 @@ func txNewCryptoAddressFullNode(ctx context.Context, db bank.Database, chainHand
 }
 
 func txNewCryptoAddressSsm(ctx context.Context, db bank.Database, chainHandler ChainHandler, chain model.String, accountID model.AccountID) (model.CryptoAddress, error) {
-	log := logger.Logger(ctx).WithField("Method", "wallet.txNewCryptoAddressFullNode")
+	log := logger.Logger(ctx).WithField("Method", "wallet.txNewCryptoAddressSsm")
 	account := genAccountLabelFromAccountID(accountID)
 
 	ssmChain := convertToSsmChain(chain)
@@ -188,7 +188,8 @@ func txNewCryptoAddressSsm(ctx context.Context, db bank.Database, chainHandler C
 	}
 
 	var result model.CryptoAddress
-	err := db.Transaction(func(db bank.Database) error {
+	// already within a db transaction
+	err := func(db bank.Database) error {
 
 		ssmAddressID, err := database.NextSsmAddressID(db, ssmChain, fingerprint)
 		if err != nil {
@@ -243,10 +244,10 @@ func txNewCryptoAddressSsm(ctx context.Context, db bank.Database, chainHandler C
 		}
 
 		return nil
-	})
+	}(db)
 	if err != nil {
 		log.WithError(err).
-			Error("Database Transaction failed")
+			Error("Failed to generate new CryptoAddress with Ssm")
 		return model.CryptoAddress{}, ErrGenAddress
 	}
 
