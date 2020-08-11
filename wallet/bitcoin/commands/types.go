a0f8ebc2c307006d828c6e7aea19c0ac792a9aa8
Michel Foucault <michel@condensat.tech>
2020-08-11 14:00:00 +0000

    Add IncludeWatching to FundRawTransaction options
    - use watchonly flag while funding transaction for SSM

--- wallet/bitcoin/commands/types.go
+++ wallet/bitcoin/commands/types.go
@@ -119,6 +119,7 @@ type RawTransaction struct {
 type FundRawTransactionOptions struct {
 	ChangeAddress          string `json:"changeAddress"`
 	ChangePosition         int    `json:"changePosition"`
+	IncludeWatching        bool   `json:"includeWatching"`
 	SubtractFeeFromOutputs []int  `json:"subtractFeeFromOutputs"`
 }
 
