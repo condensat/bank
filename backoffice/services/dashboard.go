c9ca4002983bff65ff322a57b094d8948e309923
Michel Foucault <michel@condensat.tech>
2020-09-23 13:00:00 +0000

    Add Connected user to Dashboard status
    - Add Session Count
    - Add sessions to context
    - Update UsersStatus response

--- backoffice/services/dashboard.go
+++ backoffice/services/dashboard.go
@@ -4,8 +4,10 @@ import (
 	"net/http"
 
 	apiservice "git.condensat.tech/bank/api/services"
+	"git.condensat.tech/bank/api/sessions"
 	"git.condensat.tech/bank/appcontext"
 	"git.condensat.tech/bank/database"
+	"github.com/sirupsen/logrus"
 
 	"git.condensat.tech/bank/logger"
 )
@@ -18,7 +20,8 @@ type StatusRequest struct {
 }
 
 type UsersStatus struct {
-	Count int `json:"count"`
+	Count     int `json:"count"`
+	Connected int `json:"connected"`
 }
 
 // StatusResponse holds args for string requests
@@ -28,21 +31,35 @@ type StatusResponse struct {
 
 func (p *DashboardService) Status(r *http.Request, request *StatusRequest, reply *StatusResponse) error {
 	ctx := r.Context()
-	db := appcontext.Database(ctx)
 	log := logger.Logger(ctx).WithField("Method", "services.DashboardService.Status")
 	log = apiservice.GetServiceRequestLog(log, r, "Dashboard", "Status")
+
+	db := appcontext.Database(ctx)
+	session, err := sessions.ContextSession(ctx)
+	if err != nil {
+		return apiservice.ErrServiceInternalError
+	}
 
 	userCount, err := database.UserCount(db)
 	if err != nil {
 		return apiservice.ErrServiceInternalError
 	}
+	sessionCount, err := session.Count(ctx)
+	if err != nil {
+		return apiservice.ErrServiceInternalError
+	}
+
 	*reply = StatusResponse{
 		Users: UsersStatus{
-			Count: userCount,
+			Count:     userCount,
+			Connected: sessionCount,
 		},
 	}
 
-	log.Info("Status")
+	log.WithFields(logrus.Fields{
+		"UserCount":    userCount,
+		"SessionCount": sessionCount,
+	}).Info("Status")
 
 	return nil
 }
