c2c24db5d8450f502afbc9c268140e61a3723888
Sosthène <contact@sosthene.net>
2020-09-29 11:51:50 +0200

    Modify the workflow of RawTransaction() Add a KeyIssueAsset value in context Add asset issuance in the tx generation

--- wallet/bitcoin/cmd/cli/main.go
+++ wallet/bitcoin/cmd/cli/main.go
@@ -13,23 +13,33 @@ import (
 	dotenv "github.com/joho/godotenv"
 )
 
+const (
+	KeyIssueAsset = "Key.IssueAsset"
+)
+
 func init() {
 	_ = dotenv.Load()
 }
 
 func main() {
 	ctx := context.Background()
+	ctx = context.WithValue(ctx, KeyIssueAsset, true)
 	RawTransaction(ctx)
 }
 
 func RawTransaction(ctx context.Context) {
+	var isAssetIssuance bool
+	switch ctxValue := ctx.Value(KeyIssueAsset).(type) {
+	case bool:
+		isAssetIssuance = ctxValue
+	}
 	rpcClient := bitcoinRpcClient("bitcoin-testnet", 18332)
 	if rpcClient == nil {
 		panic("Invalid rpcClient")
 	}
 
 	hex, err := commands.CreateRawTransaction(ctx, rpcClient, nil, []commands.SpendInfo{
-		{Address: "tb1qqjv0dec9vagycgwpchdkxsnapl9uy92dek4nau", Amount: 0.000003},
+		{Address: "AzpjyAMVQzUvJjpE3TbEZ1ATvTScw5poMndTvGjnnku8LtAjnh5q693iZWCvQjYKcFYJwKqY2njnvBM5", Amount: 0.003},
 	}, nil)
 	if err != nil {
 		panic(err)
@@ -77,26 +87,71 @@ func RawTransaction(ctx context.Context) {
 		}
 	}
 
-	blinded, err := commands.BlindRawTransaction(ctx, rpcClient, commands.Transaction(funded.Hex))
-	if err != nil {
-		panic(err)
+	switch {
+	case isAssetIssuance == true:
+
+		issued, err := commands.RawIssueAssetWithContract(
+			ctx,
+			rpcClient,
+			commands.Transaction(funded.Hex),
+			1000.00000001,
+			0.00000001,
+			"AzpuKUXvtnbf5uGQrpvDCYrnPJPCaYbRCLk39o73zJxq4od1u9jbcokao9fvFJQp4D9iSMUpLiuknSmR",
+			"el1qq0kw2e8g4yknjc0uggucchpucpcut47cl2xeymmqylt5vw6pt9dgl5pf9je6yxjcx7tw6ewmfv5hj54009d2yzgyqyyuulxlf",
+			"7F6475E61926B63C190CEBAB3470531EAB53B5481CBF960C3EE3164CA71E816B",
+		)
+		if err != nil {
+			panic(err)
+		}
+
+		log.Printf("Issued asset %s\n", issued.Asset)
+		log.Printf("Issued token %s\n", issued.Token)
+
+		blinded, err := commands.BlindRawTransaction(ctx, rpcClient, commands.Transaction(issued.Hex))
+		if err != nil {
+			panic(err)
+		}
+
+		log.Printf("Blinded transaction OK\n")
+
+		signed, err := commands.SignRawTransactionWithWallet(ctx, rpcClient, commands.Transaction(blinded))
+		if err != nil {
+			panic(err)
+		}
+		if !signed.Complete {
+			panic("SignRawTransactionWithWallet failed")
+		}
+
+		accepted, err := commands.TestMempoolAccept(ctx, rpcClient, signed.Hex)
+		if err != nil {
+			panic(err)
+		}
+		log.Printf("Accepted in the mempool: %+v\n", accepted.Allowed)
+
+	default:
+		blinded, err := commands.BlindRawTransaction(ctx, rpcClient, commands.Transaction(funded.Hex))
+		if err != nil {
+			panic(err)
+		}
+
+		log.Printf("Blinded transaction OK\n")
+
+		signed, err := commands.SignRawTransactionWithWallet(ctx, rpcClient, commands.Transaction(blinded))
+		if err != nil {
+			panic(err)
+		}
+		if !signed.Complete {
+			panic("SignRawTransactionWithWallet failed")
+		}
+
+		accepted, err := commands.TestMempoolAccept(ctx, rpcClient, signed.Hex)
+		if err != nil {
+			panic(err)
+		}
+		log.Printf("Accepted in the mempool: %+v\n", accepted.Allowed)
+
 	}
 
-	log.Printf("Blinded transaction OK")
-
-	signed, err := commands.SignRawTransactionWithWallet(ctx, rpcClient, commands.Transaction(blinded))
-	if err != nil {
-		panic(err)
-	}
-	if !signed.Complete {
-		panic("SignRawTransactionWithWallet failed")
-	}
-
-	accepted, err := commands.TestMempoolAccept(ctx, rpcClient, signed.Hex)
-	if err != nil {
-		panic(err)
-	}
-	log.Printf("Accepted in the mempool: %+v\n", accepted.Allowed)
 }
 
 func bitcoinRpcClient(hostname string, port int) commands.RpcClient {
