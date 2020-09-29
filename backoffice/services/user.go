f27784352cc7587bfb6225d3db28064fc99fade5
Michel Foucault <michel@condensat.tech>
2020-09-29 13:59:59 +0000

    Add UserList request handler
    - add RequestPaging
    - response return slice of userIds (secureId)

--- backoffice/services/user.go
+++ backoffice/services/user.go
@@ -2,12 +2,61 @@ package services
 
 import (
 	"context"
+	"net/http"
 
 	"git.condensat.tech/bank/appcontext"
 	"git.condensat.tech/bank/database"
+	"git.condensat.tech/bank/database/model"
+	"git.condensat.tech/bank/logger"
 
+	apiservice "git.condensat.tech/bank/api/services"
 	"git.condensat.tech/bank/api/sessions"
 )
+
+// UserListRequest holds args for userlist requests
+type UserListRequest struct {
+	RequestPaging
+	apiservice.SessionArgs
+}
+
+type UserInfo struct {
+	UserID string `json:"userId"`
+}
+
+// UserListResponse holds response for userlist request
+type UserListResponse struct {
+	RequestPaging
+	Users []UserInfo `json:"users"`
+}
+
+func (p *DashboardService) UserList(r *http.Request, request *UserListRequest, reply *UserListResponse) error {
+	ctx := r.Context()
+	log := logger.Logger(ctx).WithField("Method", "services.DashboardService.UserList")
+	log = apiservice.GetServiceRequestLog(log, r, "Dashboard", "UserList")
+
+	// Get userID from session
+	request.SessionID = apiservice.GetSessionCookie(r)
+	sessionID := sessions.SessionID(request.SessionID)
+
+	isAdmin, log, err := isUserAdmin(ctx, log, sessionID)
+	if err != nil {
+		log.WithError(err).
+			WithField("RoleName", model.RoleNameAdmin).
+			Error("UserHasRole failed")
+		return ErrPermissionDenied
+	}
+	if !isAdmin {
+		log.WithError(err).
+			Error("User is not Admin")
+		return ErrPermissionDenied
+	}
+
+	*reply = UserListResponse{
+		RequestPaging: RequestPaging{Page: request.Page, PageCount: 42},
+	}
+
+	return nil
+}
 
 type UsersStatus struct {
 	Count     int `json:"count"`
