e0c9431e07a678184933a391ce98ddf65903b811
Michel Foucault <michel@condensat.tech>
2020-10-02 12:00:00 +0000

    Add SwapList to dashboard Api
    - With paging

--- backoffice/cmd/backoffice-cli/main.go
+++ backoffice/cmd/backoffice-cli/main.go
@@ -54,6 +54,7 @@ func main() {
 	DepositList(ctx)
 	BatchList(ctx)
 	WithdrawList(ctx)
+	SwapList(ctx)
 }
 
 func migrateDatabase(ctx context.Context) {
@@ -159,4 +160,14 @@ func WithdrawList(ctx context.Context) {
 		panic(err)
 	}
 	fmt.Printf("Withdraw Page: %d\n", page)
+}
+
+func SwapList(ctx context.Context) {
+	db := appcontext.Database(ctx)
+
+	page, err := database.SwapPage(db, 0, 10)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Swap Page: %d\n", page)
 }
