abc40be88c961516d7047c08bb568ad9d4cf348e
Sosthène <contact@sosthene.net>
2020-10-13 15:09:27 +0200

    Add RawReissueAsset command

--- wallet/common/interfaces.go
+++ wallet/common/interfaces.go
@@ -16,6 +16,7 @@ type ChainClient interface {
 	GetBlockCount(ctx context.Context) (int64, error)
 	ListUnspent(ctx context.Context, minConf, maxConf int, addresses ...string) ([]TransactionInfo, error)
 	ListUnspentByAsset(ctx context.Context, minConf, maxConf int, asset string) ([]TransactionInfo, error)
+	ListUnspentWithAssetWithMaxCount(ctx context.Context, minConf, maxConf int, asset string, maxCount int) ([]TransactionInfo, error)
 	LockUnspent(ctx context.Context, unlock bool, transactions ...TransactionInfo) error
 	ListLockUnspent(ctx context.Context) ([]TransactionInfo, error)
 	GetTransaction(ctx context.Context, txID string) (TransactionInfo, error)
@@ -23,6 +24,8 @@ type ChainClient interface {
 	SpendFunds(ctx context.Context, changeAddress string, inputs []UTXOInfo, outputs []SpendInfo, addressInfo GetAddressInfo, blindTransaction bool) (SpendTx, error)
 
 	IssueNewAsset(ctx context.Context, changeAddress string, outputs SpendInfo, request IssuanceRequest, addressInfo GetAddressInfo, blindTransaction bool) (IssuanceResponse, error)
+	ListIssuances(ctx context.Context, asset string) ([]IssuanceInfo, error)
+	ReissueAsset(ctx context.Context, changeAddress string, input UTXOInfo, request ReissuanceRequest, addressInfo GetAddressInfo, blindTransaction bool) (ReissuanceResponse, error)
 }
 
 // SsmClient interface specification for crypto-ssm
