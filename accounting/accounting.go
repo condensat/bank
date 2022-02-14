7bd11b6a7cd5696a83b864b76ff94418270a4ca5
Sosthène <contact@sosthene.net>
2022-02-14 15:02:56 +0100

    Add bool to toggle off auto crypto withdraw

--- accounting/accounting.go
+++ accounting/accounting.go
@@ -30,13 +30,15 @@ func (p *Accounting) Run(ctx context.Context, bankUser model.User) {
 	ctx = common.BankUserContext(ctx, bankUser)
 	ctx = cache.RedisMutexContext(ctx)
 
+	autoBatch := false
+
 	p.registerHandlers(ctx)
 
 	log.WithFields(logrus.Fields{
 		"Hostname": utils.Hostname(),
 	}).Info("Accounting Service started")
 
-	go p.scheduledWithdrawBatch(ctx, DefaultInterval, DefaultDelay)
+	go p.scheduledWithdrawBatch(ctx, DefaultInterval, DefaultDelay, autoBatch)
 
 	<-ctx.Done()
 }
@@ -88,7 +90,7 @@ func checkParams(interval time.Duration, delay time.Duration) (time.Duration, ti
 	return interval, delay
 }
 
-func (p *Accounting) scheduledWithdrawBatch(ctx context.Context, interval time.Duration, delay time.Duration) {
+func (p *Accounting) scheduledWithdrawBatch(ctx context.Context, interval time.Duration, delay time.Duration, autoBatch bool) {
 	log := logger.Logger(ctx).WithField("Method", "Accounting.scheduledWithdrawBatch")
 
 	interval, delay = checkParams(interval, delay)
@@ -118,18 +120,20 @@ func (p *Accounting) scheduledWithdrawBatch(ctx context.Context, interval time.D
 					"Epoch": epoch.Truncate(time.Millisecond),
 				})
 
-				err := processPendingWithdraws(ctx)
-				if err != nil {
-					log.WithError(err).
-						Error("Failed to processPendingWithdraws")
-					// continue to next task
-				}
-
 				err = processCancelingWithdraws(ctx)
 				if err != nil {
 					log.WithError(err).
 						Error("Failed to processCancelingWithdraws")
 					// continue to next task
+				}
+
+				if autoBatch {
+					err := processPendingWithdraws(ctx)
+				if err != nil {
+					log.WithError(err).
+							Error("Failed to processPendingWithdraws")
+					// continue to next task
+					}
 				}
 
 				err = processPendingBatches(ctx)
